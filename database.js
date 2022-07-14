770de022b5e19f872ff4c10ffd541e192aee415e
sahidularif <sahidularif1@gmail.com>
2022-07-15 02:14:25 +0600

    Add env

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
 // MongoDB Configuration
 require('dotenv').config();
 const mongoose = require('mongoose');
+const DB = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.or4h7.mongodb.net/renterbd?retryWrites=true&w=majority`
 module.exports = () => {
-    mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.or4h7.mongodb.net/?retryWrites=true&w=majority`, {
+    mongoose.connect(DB, {
         useNewUrlParser: true,
+        // useCreateIndex: true,
         useUnifiedTopology: true,
+        // useFindAndModify: false
     })
     .then(() => console.log('Connected to Mongodb database'))
     .catch(error => console.log('Database could not be connected'))
